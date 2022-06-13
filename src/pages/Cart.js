3841e3e455fe2e38f04bd3ab367f9d747b2417f2
Cezar <cezarguimaraes@gmail.com>
2022-06-13 14:51:39 -0300

    Requerements 1 and 2 done

---
+++ src/pages/Cart.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Cart() {
+  return (
+    <p>Eu sou Cart</p>
+  );
+}
+
+export default Cart;
---
+++ src/pages/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <body>
+      <input
+        type="text"
+      />
+      <h1 data-testid="home-initial-message">
+        Digite algum termo de pesquisa ou escolha uma categoria.
+      </h1>
+    </body>
+  );
+}
+
+export default Home;
