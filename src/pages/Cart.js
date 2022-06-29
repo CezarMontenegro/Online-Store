5adccf40e8aeea16d34c03e30ec607a61a607c3a
Cezar <cezarguimaraes@gmail.com>
2022-06-29 15:17:47 -0300

    cart is all done

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,17 +1,34 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { CartContext } from '../context/CartContex';
 import undoIcon from '../images/icons8-reply-arrow-50.png';
 import cartIcon from '../images/icons8-shopping-cart-50.png';
+import xIcon from '../images/x.png';
 import openBoxIcon from '../images/icons8-box-128.png';
+import minusIcon from '../images/minus.png';
+import plusIcon from '../images/add.png';
 import '../styles/cart.css';
 
 function Cart() {
   const [cartList, setCartList] = useContext(CartContext);
 
+  // fetch functions
+
+  function fetchLocalStorage() {
+    const result = JSON.parse(localStorage.getItem('cartList'));
+    const localStorageCartList = result || [];
+    setCartList(localStorageCartList);
+  }
+
+  useEffect(() => {
+    fetchLocalStorage();
+  }, []);
+
+  // handle input functions
+
   function removeItem(event) {
     const editCartList = [...cartList];
-    editCartList.splice(event.target.value, 1);
+    editCartList.splice(event.target.parentElement.value, 1);
 
     localStorage.setItem('cartList', JSON.stringify(editCartList));
     setCartList(editCartList);
@@ -19,31 +36,34 @@ function Cart() {
 
   function increaseQuantity(event) {
     const editCartList = [...cartList];
-    editCartList[event.target.value].quantity += 1;
+    editCartList[event.target.parentElement.value].quantity += 1;
 
-    setCartList(editCartList);
     localStorage.setItem('cartList', JSON.stringify(editCartList));
+    setCartList(editCartList);
   }
 
   function decreaseQuantity(event) {
     const editCartList = [...cartList];
-    editCartList[event.target.value].quantity -= 1;
+    editCartList[event.target.parentElement.value].quantity -= 1;
 
     localStorage.setItem('cartList', JSON.stringify(editCartList));
     setCartList(editCartList);
   }
 
+  function total() {
+    const totalSum = cartList.reduce((acc, curr) => {
+      acc += (curr.price * curr.quantity);
+      return acc;
+    }, 0);
+    return `R$${totalSum.toFixed(2)}`;
+  }
+
   return (
     <div id="cart">
-      {console.log(cartList)}
-      <div>
+      <header>
         <Link to="/">
           <img src={ undoIcon } alt="undoIcon" />
         </Link>
-      </div>
-      <header>
-        <img src={ cartIcon } alt="cartIcon" />
-        <h2>Carrinho de Compras</h2>
       </header>
       <main>
         { !cartList.length
@@ -54,6 +74,10 @@ function Cart() {
             </div>)
           : (
             <div id="cart-products">
+              <div id="cart-products-title">
+                <img src={ cartIcon } alt="cartIcon" />
+                <h2>Carrinho de Compras:</h2>
+              </div>
               { cartList.map((product, index) => (
                 <div className="product-card" key={ product.id }>
                   <div className="product-card-btn">
@@ -62,7 +86,7 @@ function Cart() {
                       value={ index }
                       onClick={ (event) => removeItem(event) }
                     >
-                      X
+                      <img src={ xIcon } alt="x" height="30px" />
                     </button>
                   </div>
                   <div className="product-card-img">
@@ -77,7 +101,7 @@ function Cart() {
                       value={ index }
                       onClick={ (event) => decreaseQuantity(event) }
                     >
-                      -
+                      <img src={ minusIcon } alt="minus" height="20px" />
                     </button>
                     <h3 data-testid="product-card-quantity">
                       { product.quantity }
@@ -87,7 +111,7 @@ function Cart() {
                       value={ index }
                       onClick={ (event) => increaseQuantity(event) }
                     >
-                      +
+                      <img src={ plusIcon } alt="plus" height="20px" />
                     </button>
                   </div>
                   <div className="product-card-price">
@@ -97,6 +121,12 @@ function Cart() {
                   </div>
                 </div>
               ))}
+              <div id="total">
+                <div id="total-valor">
+                  <p>Valor Total da Compra:</p>
+                  <p>{total()}</p>
+                </div>
+              </div>
             </div>)}
       </main>
     </div>
