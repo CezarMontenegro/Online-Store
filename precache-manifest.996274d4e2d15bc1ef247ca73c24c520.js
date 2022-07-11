22c01e400b8350e60fc87ebbd53339c7c1b73498
Cezar <cezarguimaraes@gmail.com>
2022-07-11 13:58:44 -0300

    Updates

---
+++ precache-manifest.996274d4e2d15bc1ef247ca73c24c520.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "788005a9c1d111633ca7f9c2890742e7",
+    "url": "/Online-Store/index.html"
+  },
+  {
+    "revision": "7a7a57499db40c01ee02",
+    "url": "/Online-Store/static/css/main.d98166af.chunk.css"
+  },
+  {
+    "revision": "3d5ffc8b328f01a48f58",
+    "url": "/Online-Store/static/js/2.28ef2213.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/Online-Store/static/js/2.28ef2213.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a7a57499db40c01ee02",
+    "url": "/Online-Store/static/js/main.4efe3c21.chunk.js"
+  },
+  {
+    "revision": "b692c4d9c5923666b4ed",
+    "url": "/Online-Store/static/js/runtime-main.b7962b94.js"
+  },
+  {
+    "revision": "fc1760bddef7d9379ac830dd8e106b18",
+    "url": "/Online-Store/static/media/add.fc1760bd.png"
+  },
+  {
+    "revision": "dd287d28e09f7613e871ccb60e266c26",
+    "url": "/Online-Store/static/media/minus.dd287d28.png"
+  }
+]);
